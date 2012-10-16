2c6057093f5de61b0f43f7334a7c44343c8b0d23
mdeejay <melodydeejay@gmail.com>
2012-10-16 18:25:33 +0800

    OC to 1.7GHz

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -426,7 +426,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
 	policy->min =  350000;
-	policy->max = 1500000;
+	policy->max = 1600000;
 	policy->cur = omap_getspeed(policy->cpu);
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
